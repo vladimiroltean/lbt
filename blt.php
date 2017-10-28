33fd6070ed10faa0c9d199bad88f4d7a89539025
Vladimir Oltean <olteanv@gmail.com>
2017-10-29 02:54:12 +0300

    php: return default json if saving to file failed
    Signed-off-by: Vladimir Oltean <olteanv@gmail.com>

--- blt.php
+++ blt.php
@@ -7,8 +7,12 @@ ini_set("display_errors", 1);
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
   $json = file_get_contents('php://input');
   $obj = json_decode($json);
-  $fp = file_put_contents("blt.json", $json);
-  echo $json;
+  $fp = @file_put_contents("blt.json", $json);
+  if ($fp === FALSE) {
+    echo "{\"trafficRunning\": false, \"iperfFlows\":[], \"pingFlows\":[]}";
+  } else {
+    echo $json;
+  }
 } else if ($_SERVER['REQUEST_METHOD'] === "GET") {
   $json = @file_get_contents("blt.json");
   if ($json === FALSE) {
